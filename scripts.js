9262a8274764225b22cc0f971925a4e254fd12a7
BuildTools <unconfigured@null.spigotmc.org>
2019-04-03 22:03:21 +0200

    sas

--- scripts.js
+++ scripts.js
@@ -1,1 +1,14 @@
-sas
+var request = new XMLHttpRequest()
+
+request.open('GET', 'data.json', true)
+
+request.onload = function() {
+    var data = JSON.parse(this.response)
+
+    $.each(data, function(i, option) {
+        $('#sas').append($('<option/>').attr("value", i).text(data['class'][i]['name']));
+    });
+
+}
+
+request.send()
