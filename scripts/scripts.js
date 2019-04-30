c6318fa6913bd9ba983545944bf11f9fdc7229e1
BuildTools <unconfigured@null.spigotmc.org>
2019-05-01 00:29:25 +0200

    Updated json and fix several bugs

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,33 +1,33 @@
 //#### Funzione per il caricamento delle classi
 
 function buildClassList() {
-    
-    loadJSON('data', function(response) {
+
+    loadJSON('data', function (response) {
         var data = JSON.parse(response);
-        
-        for(var i = 0; i < Object.keys(data).length; i++) {
+
+        for (var i = 0; i < Object.keys(data).length; i++) {
             var ul = document.getElementById("classes");
-            
+
             var _category = document.createElement("t");
             _category.appendChild(document.createTextNode(data[i].category));
             _category.setAttribute("style", "color: grey; float: left; text-transform: uppercase");
 
             ul.appendChild(_category);
 
-            ul.innerHTML += "<br />";     
+            ul.innerHTML += "<br />";
 
-            for(var t = 0; t < Object.keys(data[i].classes).length; t++) {
+            for (var t = 0; t < Object.keys(data[i].classes).length; t++) {
                 var _class = document.createElement("a");
-                var _name = data[i].classes[t].name; 
+                var _name = data[i].classes[t].name;
 
                 _class.appendChild(document.createTextNode(_name));
                 _class.setAttribute("onClick", "buildTimetable(" + i + "," + t + ")");
                 _class.setAttribute("href", "#orario");
                 ul.appendChild(_class);
 
-            }     
+            }
 
-            ul.innerHTML += "<br />";  
+            ul.innerHTML += "<br />";
 
         }
 
@@ -41,15 +41,15 @@ function buildClassList() {
 
 function buildTimetable(category, cls) {
 
-    loadJSON('data', function(response) {
+    loadJSON('data', function (response) {
         var data = JSON.parse(response);
         var days = ['mon', 'tue', 'wed', 'thu', 'fri']
 
         var ul = document.getElementById("timetable");
 
         ul.innerHTML = "";
-        
-        days.forEach(function(day) {
+
+        days.forEach(function (day) {
             var stringSubjects = data[category].classes[cls].timetable[day];
             var subjects = stringSubjects.split('_');
 
@@ -60,14 +60,15 @@ function buildTimetable(category, cls) {
             ul.appendChild(div);
 
             var counter = 1;
-            subjects.forEach(function(subject) {
-            var divDay = document.getElementById(day);
+            subjects.forEach(function (subject) {
+                var divDay = document.getElementById(day);
 
-            var sub = document.createElement("a");
-            sub.setAttribute("class", "orario");
-            sub.appendChild(document.createTextNode(subject));
-            divDay.appendChild(sub);
+                var sub = document.createElement("a");
+                sub.setAttribute("class", "orario");
+                sub.appendChild(document.createTextNode(counter + "^Ora " + subject));
+                divDay.appendChild(sub);
 
+                counter++;
             });
 
             ul.innerHTML += "<br />";
@@ -82,17 +83,17 @@ function buildTimetable(category, cls) {
 
 //#### Funzione per caricare un file JSON
 
-function loadJSON(name, callback) {   
+function loadJSON(name, callback) {
     var xobj = new XMLHttpRequest();
 
     xobj.overrideMimeType("application/json");
     xobj.open('GET', name + '.json', true);
     xobj.onreadystatechange = function () {
-          if (xobj.readyState == 4 && xobj.status == "200") {
+        if (xobj.readyState == 4 && xobj.status == "200") {
             callback(xobj.responseText);
-          }
+        }
     };
-    xobj.send(null);  
+    xobj.send(null);
 
 }
 
